2eef98419e5d7ec66eaf786de7c095a8794c6ad6
Mone <mone_0711_t@moegi.waseda.jp>
2022-08-10 11:25:23 +0900

    update

--- Learn_1.py
+++ Learn_1.py
@@ -18,6 +18,7 @@ from torch.utils.data import TensorDataset
 from pathlib import Path
 from PIL import Image
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -26,48 +27,37 @@ class NeuralNetwork(nn.Module):
     def __init__(self, num_classes):
         super(NeuralNetwork, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2),
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),#filter
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
         )
         #Definition of how to classify
-        self.classifier = nn.Linear(in_features=4 * 4 * 128, out_features=num_classes)
+        self.classifier = nn.Linear(in_features=240000, out_features=num_classes)
     def forward(self, x):
         x = self.features(x) #small
         x = x.view(x.size(0), -1)
         x = self.classifier(x) #Classification
+        x = torch.nn.functional.hardsigmoid(x) 
         return x
-    
-class Create_Datasets(Dataset):
-    def __init__(self, img, position):
-        self.img = torch.stack(img)
-        self.position = torch.stack(position)         
-    def __getitem__(self, index):
-        path = self.img_paths[index]
-        pos = self.position[index]
-        return path, pos
-    def __len__(self):         
-        return len(self.img)  
-
 
 def dataload():
     lst_x = pd.read_csv('./Point_data.csv', header=None, dtype=int).iloc[:,0].tolist()
     lst_y = pd.read_csv('./Point_data.csv', header=None, dtype=int).iloc[:,1].tolist()    #print(lst)
-    #csvファイルのx座標とy座標の情報をリストに
     x_pos = []
     y_pos = []
     x_pos = lst_x
     y_pos = lst_y
-    #x_pos = lst[::2]
-    #y_pos = lst[1::2]
+    #normalization
+    x_pos = [n/800 for n in x_pos]
+    y_pos = [n/600 for n in y_pos]    
     output_data = [] 
     for i in range(len(x_pos)):
         output_data.append([x_pos[i], y_pos[i]])
@@ -82,24 +72,24 @@ def dataload():
         input_data.append(data_expanded)
         # (n_samples,height,width,channels)
     image_datas = np.concatenate(input_data,axis=0)
+    image_datas = np.transpose(image_datas, (0, 3, 1, 2))
     #print(image_datas)
     #print(input_data)
-    #バッチデータの準備儀式
-    input_data = torch.FloatTensor(input_data) 
+    input_data = torch.FloatTensor(image_datas) 
     output_data = torch.FloatTensor(output_data)
+    #print(output_data.shape)
     dataset = TensorDataset(input_data, output_data)
-    train_loader = DataLoader(dataset, batch_size=8, shuffle=True)
-    return input_data, output_data, train_loader
-    print(input_data.shape)
-    
+    train_loader = DataLoader(dataset, batch_size=10, shuffle=True)
+    return input_data, output_data, train_loader    
 
 def train(EPOCHS,input_data, train_loader, output_data):
     record_loss_train = []
     record_epoch_train = []
+    record_loss_test = []
     test_losses = []
     test_x = []
     test_y = []
-    model = NeuralNetwork(10).to(device)
+    model = NeuralNetwork(2).to(device)
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=0.01)
     for epoch in range(EPOCHS):
@@ -107,11 +97,15 @@ def train(EPOCHS,input_data, train_loader, output_data):
         loss_train = 0
         for j, xy in enumerate(train_loader):
             model = model.to(device)
-            train_input = xy[0].to(device)   
+            train_input = xy[0].to(device)#image
+            #print(train_input.shape) 
             train_output = xy[1].to(device)#ここj[0]じゃないの？
+            #print(xy[1])
+            #print("train_output.shape{}".format(train_output.shape))
             optimizer.zero_grad()
-            output = model(train_input)
-            loss = criterion(model(train_input), train_output)
+            output = model(train_input)#point
+            print("output{}".format(output))
+            loss = criterion(output, train_output)
             loss_train += loss.item()
             loss.backward()
             optimizer.step()
@@ -133,9 +127,9 @@ def train(EPOCHS,input_data, train_loader, output_data):
             loss_test += val_loss.item()
         loss_test /= j+1
         record_loss_test.append(loss_test)    
-        if epoch%10 == 0:
+        if epoch%1 == 0:
             print("epoch: {}, loss: {},  " \
-            "val_epoch: {}, val_loss: {}".format(epoch, loss_train, epoch, loss_test))
+            "val_epoch: {}, val_loss: {},loss_train: {}".format(epoch, loss_train, epoch, loss_test,loss_train))
 
     return test, test_x, test_y, output, val_output, record_loss_train, record_loss_test
 
@@ -144,12 +138,6 @@ def main():
     input_data, output_data, train_loader = dataload()
     test, test_x, test_y, output, val_output, record_loss_train, record_loss_test = train(EPOCHS,input_data, train_loader, output_data) 
     test = test.detach().numpy()
-    #output_x = []
-    #output_y = []
-    #for i in range(50):
-    #    output_x.append([open_xy[0][i][0]])
-    #    output_y.append([open_xy[0][i][1]])
-    #plt.plot(output_x, output_y, linestyle="None", linewidth=0, marker='o')
     plt.plot(test_x, test_y, linestyle="None", linewidth=0, marker='o')
     plt.show()
     plt.style.use('ggplot')
